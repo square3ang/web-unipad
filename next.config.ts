e870cf8362e3acd110a58d5b900cada826c778ca
square3ang <square3ang@gmail.com>
2025-05-06 18:33:20 +0900

    static

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  /* other config options */
 };
 
 export default nextConfig;
